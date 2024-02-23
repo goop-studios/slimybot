01c1af8a5e25251490c08f42ee54f155629930a7
h4rl <me@h4rl.xn--q9jyb4c>
2024-02-23 11:15:05 +0100

    feat(bot): :sparkles: Try to fix mkembed

--- src/commands/embed.rs
+++ src/commands/embed.rs
@@ -1,9 +1,9 @@
-use crate::{Error, Context, Data};
-use poise::serenity_prelude as serenity;
+use std::vec;
+
+use crate::{Error, ApplicationContext};
+use poise::serenity_prelude::{self as serenity, CreateEmbed, CreateEmbedAuthor};
 use serenity::Channel;
 use poise::Modal;
-
-type ApplicationContext<'a> = poise::ApplicationContext<'a, Data, Error>; 
 
 #[derive(Debug, Modal)]
 #[name = "Modal test"]
@@ -22,8 +22,34 @@ struct EmbedModal {
 /// Creates a modal for testing 
 #[poise::command(slash_command, required_permissions="ADMINISTRATOR")]
 pub async fn mkembed(ctx: ApplicationContext<'_>, _channel: Channel) -> Result<(), Error> {
-    let gotten_data = EmbedModal::execute(ctx).await?;
-    let reply = format!("got data: {:?}", gotten_data);
-    ctx.reply(reply).await?; 
+    let reply = {
+        let components = vec![serenity::CreateActionRow::Buttons(vec![
+            serenity::CreateButton::new("open_modal")
+                .label("Create Embed")
+                .style(serenity::ButtonStyle::Success),
+        ])];
+    
+        poise::CreateReply::default()
+            .embed(CreateEmbed::default()
+                .title("Embed Modal Test")
+                .author(CreateEmbedAuthor::new("Goop Studios")
+                    .url("https://goop-studios.monster"))
+                .description("This is a test of the embed modal."))
+            .components(components)
+    };
+
+    ctx.send(reply).await?;
+    
+    while let Some(mci) = serenity::ComponentInteractionCollector::new(ctx.serenity_context())
+        .timeout(std::time::Duration::from_secs(120))
+        .filter(move |mci| mci.data.custom_id == "open_modal")
+        .await
+    {
+        let data =
+            poise::execute_modal_on_component_interaction::<EmbedModal>(ctx, mci, None, None).await?;
+        println!("Got data: {:?}", data);
+    }
+
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod commands;
 pub struct Data {} // User data, which is stored and accessible in all command invocations
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, Data, Error>;
+pub type ApplicationContext<'a> = poise::ApplicationContext<'a, Data, Error>; 
 
 /// Responds with "world!"
 #[poise::command(slash_command)]
