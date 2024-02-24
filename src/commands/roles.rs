7c7e514bb0f0542d8ce4bd9453287af31dd11bc3
h4rl <me@h4rl.xn--q9jyb4c>
2024-02-24 18:08:13 +0100

    fix(Bot): I might be stupid.

--- src/commands/roles.rs
+++ src/commands/roles.rs
@@ -1,12 +1,14 @@
+use std::ops::Deref;
+
 use crate::{ApplicationContext, Data, Error, BotConfig};
 use poise::{
-    serenity_prelude::{self as serenity, Channel, ChannelId, CreateMessage, Member, Mentionable},
-    CreateReply, GuildRolesRef
+    serenity_prelude::{self as serenity, Channel, ChannelId, CreateMessage, Member, Mentionable, Role},
+    CreateReply,
 };
 
 #[poise::command(slash_command, required_permissions = "ADMINISTRATOR")]
-pub async fn create_role_menu(ctx: ApplicationContext<'_>, roles: GuildRolesRef) -> Result<(), Error> {
-    let reply = format!("roles text: {:?}", roles);
-    ctx.reply(reply);
+pub async fn create_role_menu(ctx: ApplicationContext<'_>, roles: Vec<Role>) -> Result<(), Error> {
+    let reply = format!("{:?}", roles);
+    ctx.reply(reply).await?;
     Ok(())
 }
